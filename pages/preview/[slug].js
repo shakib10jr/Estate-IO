b0569825f72d8e005fc09ab2315e60c0c27227d0
shakib10jr <shakibahmed97@gmail.com>
2026-05-08 17:20:47 -0400

    fix: remove nested style bug

--- pages/preview/[slug].js
+++ pages/preview/[slug].js
@@ -315,7 +315,7 @@ function StandardView({ lead, headline, positiveReviews, config, city }) {
         maxWidth: '1100px',
         margin: '0 auto',
         display: 'grid',
-        style={{ gridTemplateColumns: '1fr',
+        gridTemplateColumns: '1fr',
         gap: '48px',
         alignItems: 'center',
       }}>
